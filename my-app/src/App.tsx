35c97cc887990e762e8e73e791b23466c7611713
Maciej-Balcer <maciej.balcer.98@gmail.com>
2022-09-08 20:04:33 +0200

    Front end app structure

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Alert from 'react-bootstrap/Alert';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Fuck burgeramt
-          I like eating pommes with currywurst
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {[
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark',
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert with{' '}
+          <Alert.Link href="#">an example link</Alert.Link>. Give it a click if
+          you like.
+        </Alert>
+      ))}
+    </>
   );
 }
 
