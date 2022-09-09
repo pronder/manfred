9c27ff7f46c4fcb0780831e93c4a9fafa734feae
Leghart <leghart@wp.pl>
2022-09-09 16:47:37 +0200

    changed mongo to postgres

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,48 +1,23 @@
-import bodyParser from 'body-parser';
-import compression from 'compression';
-import path from 'path';
-import express, { Request, Response, NextFunction } from 'express';
-import ApplicationError from './errors/application-error';
-import routes from './routes';
-import logger from './logger';
+import { Request, Response } from 'express';
+
+const express = require('express');
 
 const app = express();
+const db = require('./queries');
 
-function logResponseTime(req: Request, res: Response, next: NextFunction) {
-  const startHrTime = process.hrtime();
+const port = 3000;
 
-  res.on('finish', () => {
-    const elapsedHrTime = process.hrtime(startHrTime);
-    const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
-    const message = `${req.method} ${res.statusCode} ${elapsedTimeInMs}ms\t${req.path}`;
-    logger.log({
-      level: 'debug',
-      message,
-      consoleLoggerOptions: { label: 'API' }
-    });
-  });
 
-  next();
-}
-
-app.use(logResponseTime);
-
-app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
-
-app.use(routes);
-
-app.use((err: ApplicationError, req: Request, res: Response, next: NextFunction) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-
-  return res.status(err.status || 500).json({
-    error: err.message
-  });
+app.get('/', (request: Request, response: Response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
-export default app;
+app.get('/test', (request:Request, response:Response) => {
+  response.send('test output');
+});
+
+app.get('/users', db.getUsers);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
---
+++ backend/src/queries.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-import-module-exports
+import { Request, Response } from 'express';
+// eslint-disable-next-line import/no-import-module-exports
+import dotenv from 'dotenv';
+
+dotenv.config(); // Reads .env file and makes it accessible via process.env
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || '5432', 10)
+});
+
+const getUsers = (request:Request, response:Response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error:any, results:any) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+module.exports = {
+  getUsers
+};
