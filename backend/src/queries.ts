1b534054180aafcadf3998f471e4f326a9342f96
Leghart <leghart@wp.pl>
2022-09-11 13:17:15 +0200

    prettierignore

--- backend/src/queries.ts
+++ backend/src/queries.ts
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import { Pool } from 'pg'
 
 dotenv.config()
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -16,14 +14,12 @@ const pool = new Pool({
 
 // eslint-disable-next-line import/prefer-default-export
 export const getUsers = (request: Request, response: Response) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     pool.query(
         'SELECT * FROM users ORDER BY id ASC',
         (error: Error, results) => {
             if (error) {
                 throw error
             }
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             response.status(200).json(results.rows)
         }
     )
