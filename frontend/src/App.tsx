1f3db172ae42b0c499e023399ea47684670a3fe0
Maciej-Balcer <maciej.balcer.98@gmail.com>
2022-09-17 12:37:41 +0200

    linters and formaters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Alert from 'react-bootstrap/Alert'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Fuck burgeramt
-          I like eating pommes with currywurst
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            {[
+                'primary',
+                'secondary',
+                'success',
+                'danger',
+                'warning',
+                'info',
+                'light',
+                'dark',
+            ].map((variant) => (
+                <Alert key={variant} variant={variant}>
+                    This is a {variant} alert—check it out!
+                </Alert>
+            ))}
+        </>
+    )
 }
 
-export default App;
+export default App
